b251e6616dc8606797c1735cede7b0b6ac3ba9fb
Vladimir Rodionov <vsrodionov94@yandex.ru>
2022-06-13 16:45:02 +0700

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const checkUser = require('./checkUser');
 const getRaitings = require('./getRaitings');
+const tryHitting = require('./tryHitting');
 
 module.exports = app => {
   checkUser(app);
   getRaitings(app);
+  tryHitting(app);
 };
